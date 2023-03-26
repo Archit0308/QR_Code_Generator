6bfc9ced1532104a2c7d28dd251ed935c9e25235
ARCHIT <127728488+Archit0308@users.noreply.github.com>
2023-03-26 22:41:49 +0530

    Update QR.js

--- QR.js
+++ QR.js
@@ -1,24 +1,27 @@
-const wrapper = document.querySelector(".wrapper"),
-qrInput = wrapper.querySelector(".form input"),
-generateBtn = wrapper.querySelector(".form button"),
-qrImg = wrapper.querySelector(".qr-code img");
+const wrapper = document.querySelector(".wrapper");
+const qrInput = wrapper.querySelector(".form input");
+const generateBtn = wrapper.querySelector(".form button");
+const qrImg = wrapper.querySelector(".qr-code img");
 let preValue;
 
 generateBtn.addEventListener("click", () => {
-    let qrValue = qrInput.value.trim();
-    if(!qrValue || preValue === qrValue) return;
-    preValue = qrValue;
-    generateBtn.innerText = "Generating QR Code...";
-    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
-    qrImg.addEventListener("load", () => {
-        wrapper.classList.add("active");
-        generateBtn.innerText = "Generate QR Code";
-    });
+  const qrValue = qrInput.value.trim();
+  if (!qrValue || preValue === qrValue) return;
+
+  preValue = qrValue;
+  generateBtn.innerText = "Generating QR Code...";
+
+  qrImg.addEventListener("load", () => {
+    wrapper.classList.add("active");
+    generateBtn.innerText = "Generate QR Code";
+  });
+
+  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
 });
 
 qrInput.addEventListener("keyup", () => {
-    if(!qrInput.value.trim()) {
-        wrapper.classList.remove("active");
-        preValue = "";
-    }
+  if (!qrInput.value.trim()) {
+    wrapper.classList.remove("active");
+    preValue = "";
+  }
 });
